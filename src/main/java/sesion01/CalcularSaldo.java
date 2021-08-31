a96237fde9ef6ee8303cb6ec1a28995c0f96c447
Danil <jhon3694@gmail.com>
2021-08-31 16:04:32 -0500

    CalcularSaldo commit1

--- src/main/java/sesion01/CalcularSaldo.java
+++ src/main/java/sesion01/CalcularSaldo.java
@@ -1,2 +1,14 @@
-package sesion01;public class CalcularSaldo {
+package sesion01;
+
+
+
+public class CalcularSaldo {
+
+    public static void  main(String[] args){
+        System.out.println("Hola Bienvenido al mundo de Java for Testing");
+        System.out.println("Hello word");
+
+
+
+    }
 }
